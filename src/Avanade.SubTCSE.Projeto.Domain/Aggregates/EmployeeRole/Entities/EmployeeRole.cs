e56d1ec4ccfefd59df4327d6b0fe92a50100400a
Robson Mendonça <rob.ams@gmail.com>
2021-06-15 09:02:12 -0300

    Create a domain - employeeRole.

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,16 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public string Id { get; private set; }
+
+        public string RoleName { get; init; }
+
+    }
+}
