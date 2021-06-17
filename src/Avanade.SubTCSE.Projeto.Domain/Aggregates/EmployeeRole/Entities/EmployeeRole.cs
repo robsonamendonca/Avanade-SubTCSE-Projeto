3510ea51f90d14906647799d6f45aeaf1a9b167b
Robson Mendonça <rob.ams@gmail.com>
2021-06-17 08:59:32 -0300

    Aula 4

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -8,6 +8,11 @@
             RoleName = roleName;
         }
 
+        public EmployeeRole(string roleName)
+        {
+            RoleName = roleName;
+        }
+
         public string Id { get; private set; }
 
         public string RoleName { get; init; }
---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services
+{
+    public interface IEmployeeRoleService
+    {
+        void AddEmployeeRole(Entities.EmployeeRole employeeRole);
+    }
+}
