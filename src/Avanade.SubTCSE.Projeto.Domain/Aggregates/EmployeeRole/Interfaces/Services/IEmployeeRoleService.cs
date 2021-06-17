4cde5b1ae8267d0132a5c58c728074266cb6af30
Robson Mendonça <rob.ams@gmail.com>
2021-06-17 09:01:45 -0300

    Aula 4 salvando...

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services
 {
     public interface IEmployeeRoleService
     {
